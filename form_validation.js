39aafc11f16887e9a31794ea2640c3a6ec4334d0
nehasukhnani18 <nehasukhnani@lusakaoaktree.school>
2025-11-21 16:47:40 +0200

    form validation

---
+++ form_validation.js
@@ -0,0 +1,22 @@
+function validate(e){
+    e.preventDefault();
+    const email=document.getElementById('email').value;
+    const pass=document.getElementById('password').vaule;
+    const age=document.getElementById('age').value;
+    const msgBox=document.getElementById('message');
+    let message='';
+    if (email==='') {
+        message='enter a email.';
+        msgBox.style.color='red';
+    } else if(password===''){
+        message='enter a password.';
+        msgBox.style.color='red';
+    }else if(age===''){
+        message='enter your age.';
+        msgBox.style.color='red';
+    }else{
+        message ='login successful!';
+        msgBox.style.color='green';
+    }
+    msgBox.innerText=message;
+}
